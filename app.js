d5ef4c82aec159352393883dec14e9e22e4ebec0
albertxavier001 <albertxavier001@gmail.com>
2016-10-21 13:16:39 +0800

    1111

--- app.js
+++ app.js
@@ -38,17 +38,17 @@ app.use(express.session({
 // 请参考 http://expressjs.com/2x/guide.html#session-support
 
 // 在环境变量提供的 $PORT 或 3000 端口监听
-var port = process.env.PORT || 26881;
-app.listen(port, function(){
-  log("Listening on %s", port);
-});
+// var port = process.env.PORT || 26881;
+// app.listen(port, function(){
+//   log("Listening on %s", port);
+// });
 
 // 微信接口地址只允许服务放在 80 端口
 // 所以需要做一层 proxy
-app.enable('trust proxy');
+// app.enable('trust proxy');
 
 // 当然，如果你的服务器允许，你也可以直接用 node 来 serve 80 端口
-// app.listen(80);
+app.listen(80);
 
 if(!process.env.DEBUG){
   console.log("set env variable `DEBUG=webot-example:*` to display debug info.");
