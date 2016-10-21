5b1dd277f31c135e69622e8cfd8bd7209ec452b8
albertxavier001 <albertxavier001@gmail.com>
2016-10-21 23:12:54 +0800

    add menu 6

--- app.js
+++ app.js
@@ -64,33 +64,33 @@ api.createMenu(menu, function (err, res) {
 
 // 启动服务
 
-// 实际使用时，这里填写你在微信公共平台后台填写的 token
-var wx_token = process.env.WX_TOKEN || token;
-// var wx_token2 = process.env.WX_TOKEN_2 || 'albertxavierALBERT111';
-
-// 建立多个实例，并监听到不同 path ，
-// var webot2 = new webot.Webot();
-
-// 载入webot1的回复规则
-require('./rules')(webot);
-// 为webot2也指定规则
-webot.set('1', 'hi..');
-
-// 启动机器人, 接管 web 服务请求
-// webot.watch(app, { token: wx_token, path: '/wechat' });
-// 若省略 path 参数，会监听到根目录
-webot.watch(app, { token: wx_token });
-
-// 后面指定的 path 不可为前面实例的子目录
-// webot2.watch(app, { token: wx_token2, path: '/wechat_2' });
-
-// 如果需要 session 支持，sessionStore 必须放在 watch 之后
-app.use(express.cookieParser());
-// 为了使用 waitRule 功能，需要增加 session 支持
-app.use(express.session({
-  secret: 'abced111',
-  store: new express.session.MemoryStore()
-}));
+// // 实际使用时，这里填写你在微信公共平台后台填写的 token
+// var wx_token = process.env.WX_TOKEN || token;
+// // var wx_token2 = process.env.WX_TOKEN_2 || 'albertxavierALBERT111';
+//
+// // 建立多个实例，并监听到不同 path ，
+// // var webot2 = new webot.Webot();
+//
+// // 载入webot1的回复规则
+// require('./rules')(webot);
+// // 为webot2也指定规则
+// webot.set('1', 'hi..');
+//
+// // 启动机器人, 接管 web 服务请求
+// // webot.watch(app, { token: wx_token, path: '/wechat' });
+// // 若省略 path 参数，会监听到根目录
+// webot.watch(app, { token: wx_token });
+//
+// // 后面指定的 path 不可为前面实例的子目录
+// // webot2.watch(app, { token: wx_token2, path: '/wechat_2' });
+//
+// // 如果需要 session 支持，sessionStore 必须放在 watch 之后
+// app.use(express.cookieParser());
+// // 为了使用 waitRule 功能，需要增加 session 支持
+// app.use(express.session({
+//   secret: 'abced111',
+//   store: new express.session.MemoryStore()
+// }));
 // 在生产环境，你应该将此处的 store 换为某种永久存储。
 // 请参考 http://expressjs.com/2x/guide.html#session-support
 
